5534ac008190383150d1f88bc6e44fca3df0bb98
nderr <nicholas.derr@gmail.com>
2015-04-07 17:07:15 -0500

    included GSON as referenced library, updated collection of stars

---
+++ Constellate/src/com/nderr/jconstellate/SkyPlot.java
@@ -0,0 +1,170 @@
+package com.nderr.jconstellate;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import com.google.gson.Gson;
+
+/**
+ * Driver class for rough plotting using JFreeChart
+ * @author nderr
+ */
+public class SkyPlot {
+	
+	/**
+	 * Reads in data from hygdata_v3.csv and plots star positions at Madison
+	 */
+	public static void main(String[] args) {
+		long t1 = System.currentTimeMillis();
+		Gson gson = new Gson();
+		Scanner s = null;
+		try {
+			s = new Scanner(new File("stars.json"));
+		} catch (IOException e) {
+			System.out.println("Problem with stars.json");
+			System.exit(-1);
+		}
+		
+		List<Star> stars = new ArrayList<Star>();
+		while (s.hasNextLine()) {
+			stars.add(gson.fromJson(s.nextLine(),Star.class));
+		}
+		System.out.println("loaded " + stars.size() + " stars");
+		long t2 = System.currentTimeMillis();
+		
+		try {
+			s = new Scanner(new File("const.json"));
+		} catch (IOException e) {
+			System.out.println("Problem with const.json");
+			System.exit(-1);
+		}
+		
+		List<Constellation> cons = new ArrayList<Constellation>();
+		while (s.hasNextLine()) {
+			cons.add(gson.fromJson(s.nextLine(),Constellation.class));
+		}
+		System.out.println("loaded " + cons.size() + " constellations");
+		long t3 = System.currentTimeMillis();
+		
+		final Sky sky = new Sky(stars,cons,6.5);
+		
+		sky.addViewConst();
+		
+		System.out.println("built sky");
+		
+		long t4 = System.currentTimeMillis();
+		
+		CoordTrans ct = new CoordTrans(43.07*Math.PI/180,-89.4*Math.PI/180,Math.PI/7,0,0,Math.PI/1.5);
+		
+		sky.lookAt(ct);
+		
+		System.out.println("pointed to FOV");
+		
+		long t5 = System.currentTimeMillis();
+		
+		final SkyPlot sp = new SkyPlot();
+		SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                Plot p = sp.new Plot(sky);
+                p.setVisible(true);
+            }
+        });
+		
+		System.out.println("plotted FOV");
+		
+		long t6 = System.currentTimeMillis();
+		int d1 = (int) (t2 - t1);
+		int d2 = (int) (t3 - t2);
+		int d3 = (int) (t4 - t3);
+		int d4 = (int) (t5 - t4);
+		int d5 = (int) (t6 - t5);
+		System.out.println("Loading stars: " + d1 + " ms");
+		System.out.println("Loading constellations: " + d2 + " ms");
+		System.out.println("Making sky: " + d3 + " ms");
+		System.out.println("Pointing to sky: " + d4 + " ms");
+		System.out.println("Plotting: " + d5 + " ms");
+	}
+	
+	@SuppressWarnings("serial")
+	class Plot extends JFrame {
+
+	    public Plot(Sky sky) {
+
+	        initUI(sky);
+	    }
+
+	    public void initUI(Sky sky) {
+	        
+	        setTitle("Points");
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	        add(new Screen(sky));
+
+	        setSize(350, 250);
+	        setLocationRelativeTo(null);
+	    }
+	}
+	
+	@SuppressWarnings("serial")
+	class Screen extends JPanel {
+		
+		private Sky sky;
+		
+		public Screen(Sky sky) {
+			this.sky = sky;
+		}
+		
+		private void doDrawing(Graphics g) {
+			Graphics2D g2d = (Graphics2D) g;
+			
+			Dimension size = getSize();
+	        Insets insets = getInsets();
+
+	        int w = size.width - insets.left - insets.right;
+	        int h = size.height - insets.top - insets.bottom;
+	        
+	        setBackground(Color.BLUE);
+	        
+	        for (Star s : sky.getStarFOV()) {
+	        	int x = s.getX(w,h);
+	        	int y = s.getY(w,h);
+	        	int r = (int) (6 - s.getMag());
+	        	g2d.setColor(Color.YELLOW);
+	        	g2d.fillOval(x-r, y-r, 2*r, 2*r);
+	        }
+	        
+	        for (Constellation c : sky.getConstFOV()) {
+	        	float[] coords = c.getLines(w,h);
+	        	for (int i = 0; i < coords.length; i += 4) {
+	        		int x1 = (int) coords[i];
+	        		int y1 = (int) coords[i + 1];
+	        		int x2 = (int) coords[i + 2];
+	        		int y2 = (int) coords[i + 3];
+	        		g2d.setColor(Color.WHITE);
+	        		g2d.drawLine(x1, y1, x2, y2);
+	        	}
+	        }
+	        
+		}
+		
+		@Override
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			doDrawing(g);
+		}
+	}
+}
---
+++ Constellate/src/com/nderr/jconstellate/Star.java
@@ -0,0 +1,319 @@
+package com.nderr.jconstellate;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+
+/**
+ * Represents a star with a unique ID number
+ * @author nderr
+ */
+public class Star implements Comparable<Star> {
+	
+	// constant unique id
+	public int ID_NUM;
+	
+	// in RADIANS
+	private double ra;
+	private double dec;
+	
+	private transient double x = 0;
+	private transient double y = 0;
+	
+	private String name;
+	private String bayer;
+	private Integer flam;
+	private String cons;
+	
+	// position on unit celestial sphere
+	private transient Vector hat = null;
+	
+	// apparent magnitude (smaller is brighter!)
+	private double mag = 0;
+	
+	/**
+	 * Empty constructor for GSON.
+	 */
+	public Star() { }
+	
+	/**
+	 * Creates star with given id, right ascension, declination, and apparent
+	 * magnitude
+	 * @param id unique ID number
+	 * @param ra right ascension
+	 * @param dec declination
+	 * @param mag apparent magnitude
+	 */
+	public Star(int id, double ra, double dec, double mag, String name, 
+			String bayer, Integer flam, String cons) {
+		this.ID_NUM = id;
+		this.ra = ra;
+		this.dec = dec;
+		this.mag = mag;
+		this.name = name;
+		this.bayer = bayer;
+		this.flam = flam;
+		this.cons = cons;
+	}
+	
+	/**
+	 * Returns whether star has a Bayer identifier
+	 */
+	public boolean hasBayer() {
+		return bayer != null;
+	}
+	
+	/**
+	 * Returns whether star has a Flamsteed identifiers
+	 */
+	public boolean hasFlamsteed() {
+		return flam != null;
+	}
+	
+	/**
+	 * Returns whether star has a proper name
+	 */
+	public boolean hasName() {
+		return name != null;
+	}
+	
+	/**
+	 * Retruns whether star has a constellation code
+	 */
+	public boolean hasCode() {
+		return cons != null;
+	}
+	
+	/**
+	 * Returns the star's proper name, or null if none
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Returns the star's constellation code, or null if none
+	 */
+	public String getCode() {
+		return cons;
+	}
+	
+	/**
+	 * Returns the star's Flamsteed identifier, or null if none
+	 */
+	public int getFlamsteed() {
+		if (flam == null)
+			return -1;
+		
+		return flam;
+	}
+	
+	/**
+	 * Returns the star's Bayer identifier, or null if none
+	 */
+	public String getBayer() {
+		return bayer;
+	}
+	
+	/**
+	 * Sets the star's hat vector.
+	 */
+	public void setHat() {
+		hat = new Vector(Math.PI/2 - dec, ra); // use Vector constructor
+	}
+	
+	/**
+	 * @return position on celestial sphere in cartesian space
+	 */
+	public Vector getHat() {
+		if (hat == null) {
+			setHat();
+		}
+		return hat;
+	}
+	
+	/**
+	 * @param v other vector
+	 * @return angular separation in radians between this 
+	 * star's direction on the celestial sphere and a provided unit vector
+	 */
+	public double angularSep(Vector v) {
+		return Math.acos(v.dot(hat));
+	}
+
+	/**
+	 * @return apparent magnitude
+	 */
+	public double getMag() {
+		return mag;
+	}
+
+	/**
+	 * For sorting. Returns -1 if this magnitude less than that (if this
+	 * star is brighter than that star)
+	 */
+	public int compareTo(Star that) {
+		if (this.mag < that.mag) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+	
+	/**
+	 * @return right ascension
+	 */
+	public double getRA() {
+		return ra;
+	}
+	
+	/**
+	 * @return declination
+	 */
+	public double getDec() {
+		return dec;
+	}
+	
+	/**
+	 * Gets x-coordinate for plotting on screen of size w x h
+	 */
+	public int getX(int w, int h) {
+		double d = Math.sqrt(Math.pow(w,2) + Math.pow(h,2));
+		return (int) ((d/2)*x + w/2);
+	}
+	
+	/**
+	 * Gets y-coordinate for plotting on screen of size w x h
+	 */
+	public int getY(int w, int h) {
+		double d = Math.sqrt(Math.pow(w,2) + Math.pow(h,2));
+		return (int) (-(d/2)*y + h/2);
+	}
+	
+	/**
+	 * Sets the normalized x and y coordinates given a coordinate transfer object
+	 */
+	public boolean setPlot(CoordTrans ct) {
+		Vector coords = ct.getXY(this);
+		x = coords.getX();
+		y = coords.getY();
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("ID : " + ID_NUM);
+		sb.append("\nRA : " + ra);
+		sb.append("\nDec: " + dec);
+		sb.append("\nMag: " + mag);
+		return sb.toString();
+	}
+	
+	/**
+	 * Writes a JSON file of star objects from hygdata_v3.csv
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// get scanner with star info
+		Scanner s = null;
+		try {
+			s = new Scanner(new File("hygdata_v3.csv"));
+			s.nextLine();
+			s.nextLine();
+		} catch (IOException e) {
+			System.out.println("Problem with hyg file");
+			System.exit(-1);
+		}
+		
+		// for star parameters
+		String[] info = null;
+		
+		// instantiate list with fields
+		List<Star> stars = new ArrayList<Star>();
+		int id;
+		String name, bayer, cons;
+		Integer flam;
+		double ra, dec, mag;
+		
+		// go through file
+		while (s.hasNextLine()) {
+			
+			// split info
+			info = s.nextLine().split(",");
+			
+			// get id
+			id = Integer.parseInt(info[0]);
+			
+			// get name
+			if (info[6].equals("")) {
+				name = null;
+			} else {
+				name = info[6];
+			}
+			
+			// get bayer
+			if (info[27].equals("")) {
+				bayer = null;
+			} else {
+				bayer = info[27];
+			}
+			
+			// get flamsteed
+			if (info[28].equals("")) {
+				flam = null;
+			} else {
+				flam = Integer.parseInt(info[28]);
+			}
+			
+			// get constellation code
+			if (info[29].equals("")) {
+				cons = null;
+			} else {
+				cons = info[29].toUpperCase();
+			}
+			
+			// get ra (in radians)
+			ra = Double.parseDouble(info[23]);
+			
+			// get dec (in radians)
+			dec = Double.parseDouble(info[24]);
+			
+			// get magnitude
+			mag = Double.parseDouble(info[13]);
+			
+			// add star to list
+			stars.add(new Star(id,ra,dec,mag,name,bayer,flam,cons));
+		}
+		
+		// use gson
+		Gson gson = new Gson();
+		
+		// start writing new JSON file
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(new File("stars.json"));
+		} catch (IOException e) {
+			System.out.println("error with file");
+			System.exit(-1);
+		}
+		
+		// write each object to json file
+		int num = 0;
+		for (Star st : stars) {
+			if (st.getMag() < 6.5) {
+				pw.println(gson.toJson(st));
+				num++;
+			}
+		}
+		pw.close();
+		System.out.println(num + " written to JSON");
+	}
+	
+}
